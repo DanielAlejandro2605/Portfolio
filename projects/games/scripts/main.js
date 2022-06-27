fbd16f9ce16b065bd229dcf2f353838d0a4e2deb
Daniel alejandro NIETO CASTRO <dnieto-c@e3r7p16.42.fr>
2022-06-27 11:08:57 +0200

    Ready to hosting

--- projects/games/scripts/main.js
+++
@@ -1,7 +0,0 @@
-const gameButtons = document.querySelectorAll('button');
-let gameSelected;
-
-gameButtons.forEach(button => addEventListener('click', (e) => {
-    gameSelected = e.target.id;
-}))
-
